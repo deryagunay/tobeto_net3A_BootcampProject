ac8f53d83f3f53cfbf03adeea3c9d0fc60985509
deryagunay <deryagunay92@outlook.com>
2024-03-07 01:44:40 +0300

    deneme

---
+++ Core/deneme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class deneme
+    {
+        public void deneme()
+        {
+            Console.WriteLine("hello");
+
+        }
+    }
+}
